2aa2cdea204217fe44413a7bd2dc45182f648ef4
KarthikBenki <karthikmc007@gmail.com>
2023-06-13 20:53:55 +0530

    create reducer function use useReducer and create context hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import './App.css';
-import ComponentA from './components/ComponentA';
-import ComponentB from './components/ComponentB';
-import ComponentC from './components/ComponentC';
+import { createContext, useReducer } from "react";
+import "./App.css";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
+import ComponentC from "./components/ComponentC";
+let initialState = 0;
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+
+    case "decrement":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+const CounterContext = createContext(); //will create a context
 
 function App() {
+  const [state, dispatch] = useReducer(counterReducer, initialState); //Use Reducer Will take reducer function and initialState
   return (
-    <div className="App">
-      <ComponentA/>
-      <ComponentB/>
-      <ComponentC/>
-    </div>
+    <CounterContext.Provider value={{ state, dispatch }}>
+      <div className="App">
+        <h1>{state}</h1>
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CounterContext.Provider>
   );
 }
 
